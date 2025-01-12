1b7a219be302bd07cf87d5a6ac88ead280a1f044
Chris Coulbeck <cdcoulbeck@gmail.com>
2025-01-13 12:44:43 +1300

    Add Todo model

---
+++ TodoConsoleApp/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoConsoleApp
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public bool IsCompleted { get; set; } = false;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
